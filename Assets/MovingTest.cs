d7e7bcc29365f4ea2d4fcc3e67ad869d8b8bf3f4
aclouie89 <43075684+aclouie89@users.noreply.github.com>
2019-10-03 15:12:58 -0500

    Initial Commit
    - He Feng's Branch
    - Stage 1
    - Menu Screen

---
+++ Assets/MovingTest.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTest : MonoBehaviour
+{
+    public float inputDelay = 0.1f;
+    public float forwardVel = 12;
+    public float rotateVel = 100;
+
+    Quaternion targetRotation;
+    Rigidbody rbody;
+
+    float forwardInput, turnInput;
+
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRotation = transform.rotation;
+        if (GetComponent<Rigidbody>())
+            rbody = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("The character needs a rigidbody");
+
+        forwardInput = turnInput = 0;
+    }
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Turn();
+    }
+
+    void FixedUpdate()
+    {
+        Run();
+    }
+
+    void Run()
+    {
+        if (Mathf.Abs(forwardInput) > inputDelay)
+        {
+            rbody.velocity = transform.forward * forwardInput * forwardVel;
+        }
+        else {
+            rbody.velocity = Vector3.zero;
+        }
+    }
+
+    void Turn()
+    {
+        targetRotation = Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up);
+        transform.rotation = targetRotation;
+    }
+}
