9097c1ae9d679ad2f072e688acc1afe2762e385c
knimushakavi <43189181+knimushakavi@users.noreply.github.com>
2019-10-04 23:56:21 -0500

    Added Changing sprite on click
    If key is pressed the sprite changes. But I need to know how the picking and dropping of object works to set the right materials. Also I need the assets for those changes.

---
+++ Assets/SpriteSwapping.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSwapping : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite WholeVeg;
+    public Sprite ChoppedVeg;
+
+    float timer = 1f;
+    float delay = 1f;
+
+    void Start()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = WholeVeg;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite == WholeVeg) {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = ChoppedVeg;
+                timer = delay;
+            }
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite == WholeVeg)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = ChoppedVeg;
+                timer = delay;
+            }
+        }
+        
+    }
+}
---
+++ Assets/SwappingMaterials.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwappingMaterials : MonoBehaviour
+{
+    public Material[] material;
+    public KeyCode key;
+    Renderer rend;
+
+    float timer = 1f;
+    float delay = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material = material[0];
+
+    }
+
+    void UnChopped() {
+        GetComponent<Renderer>().material = material[0];
+        Invoke("Chopped", 3.0f);
+
+    }
+    void Chopped()
+    {
+        GetComponent<Renderer>().material = material[1];
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            Invoke("UnChopped", 1.0f);
+        }
+    }
+}
