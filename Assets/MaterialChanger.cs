f6561a4e553119fe722b265f305acb1d4505f0ab
knimushakavi <43189181+knimushakavi@users.noreply.github.com>
2019-10-05 01:33:38 -0500

    Added material swapper
    Press the C key and and observe the cheese plate

---
+++ Assets/MaterialChanger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour
+{
+    public Material[] material;
+    public KeyCode key;
+    Renderer rend;
+
+    float timer = 1f;
+    float delay = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material = material[0];
+
+    }
+
+    void UnChopped()
+    {
+        GetComponent<Renderer>().material = material[0];
+        Invoke("Chopped", 1.0f);
+
+    }
+    void Chopped()
+    {
+        GetComponent<Renderer>().material = material[1];
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            Invoke("UnChopped", 1.0f);
+        }
+    }
+}
