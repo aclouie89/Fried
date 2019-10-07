b7f784c04bfe75167676c13c935d128c7b69284e
aclouie89 <43075684+aclouie89@users.noreply.github.com>
2019-10-07 17:28:20 -0500

    Player 2 Movement
    - Player 2 Movement added
    - Changed from getKey to getButton (getKey had a ghosting issue, the getButton does not. Change the inputs in EDIT -> Project Settings -> Input)

--- Assets/Scripts/PlayerOneControl.cs
+++ Assets/Scripts/PlayerOneControl.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerOneControl : MonoBehaviour
 {
-    CharacterController controller;
+    CharacterController p1controller;
     public float movementSpeed = 5.0F;
     public float jumpSpeed = 8.0F;
     public float gravity = 32.0F;
@@ -14,7 +14,7 @@ public class PlayerOneControl : MonoBehaviour
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        p1controller = GetComponent<CharacterController>();
     }
 
     void allowMovement(bool flag)
@@ -26,40 +26,30 @@ public class PlayerOneControl : MonoBehaviour
     void Update()
     {
         // only change movement when grounded
-        if (controller.isGrounded)
+        if (p1controller.isGrounded)
         {
             moveDirection = new Vector3(0.0f, 0.0f, 0.0f);
             // move forward
-            if (Input.GetKey("w"))
+            if (Input.GetButton("P1Vertical"))
             {
-                moveDirection.z += Input.GetAxis("Vertical");
+                moveDirection.z += Input.GetAxis("P1Vertical");
             }
-            if (Input.GetKey("s"))
+            // move sideways
+            if (Input.GetButton("P1Horizontal"))
             {
-                moveDirection.z += Input.GetAxis("Vertical");
+                moveDirection.x += Input.GetAxis("P1Horizontal");
             }
-            if (Input.GetKey("a"))
-            {
-                moveDirection.x += Input.GetAxis("Horizontal");
-            }
-            if (Input.GetKey("d"))
-            {
-                moveDirection.x += Input.GetAxis("Horizontal");
-            }
-            //if (input.GetKeyDown(KeyCode.A))
-            /*
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            */
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= movementSpeed;
-            if (Input.GetButton("Jump"))
+            /*if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
+                */
 
         }
 
         // move character
         moveDirection.y -= gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+        p1controller.Move(moveDirection * Time.deltaTime);
 
     }
 
