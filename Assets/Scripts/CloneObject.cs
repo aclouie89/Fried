5f7942a47f8a626d1893b97b5133466e76545d35
MintsF <42982813+MintsF@users.noreply.github.com>
2019-10-15 15:04:35 -0500

    add
    player1 and player2 pickup

--- Assets/Scripts/CloneObject.cs
+++ Assets/Scripts/CloneObject.cs
@@ -11,10 +11,10 @@ public class CloneObject : MonoBehaviour
    // public Transform distance1;
     GameObject clone1;
     public Transform plate;
-    bool flag = false;
+    bool flagHold = false;
     public float smoothTime = 0.001f;
     private Vector3 AVelocity = Vector3.zero;
-    bool flag1 = false;
+    //bool flagDestroy = false;
     public Transform TransCan;
     float delDis=3f;
     float putDis = 3f;
@@ -25,50 +25,69 @@ public class CloneObject : MonoBehaviour
         float dist1 = Vector3.Distance(object1.transform.position, Player.position);
         float dist2 = Vector3.Distance(object2.transform.position, Player.position);
         float dist3 = Vector3.Distance(object3.transform.position, Player.position);
-        
-        //print(dist);
-        if (Input.GetKeyDown(KeyCode.C) && !flag1 && dist1<2f)
-        {
-           clone1 = Instantiate(object1, Player.position, Quaternion.identity) as GameObject;     
-            flag = true;
-            flag1 = true;
-            //delDis= Vector3.Distance(Player.position, TransCan.position);
 
+        //print(dist);
+        if (Input.GetKeyDown(KeyCode.E) && !flagHold) {
+            if(dist1 < 2f)
+            {
+                clone1 = Instantiate(object1, Player.position, Quaternion.identity) as GameObject;
+                flagHold = true;
+
+            }else if (dist2< 2f)
+            {
+                clone1 = Instantiate(object2, Player.position, Quaternion.identity) as GameObject;
+                flagHold = true;
+            }else if(dist3 < 2f)
+            {
+                clone1 = Instantiate(object3, Player.position, Quaternion.identity) as GameObject;
+                flagHold = true;
+
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.T) && !flag1 && dist2<2f)
-        {
-            clone1 = Instantiate(object2, Player.position, Quaternion.identity) as GameObject;
-            flag = true;
-            flag1 = true;
-            //delDis = Vector3.Distance(Player.position, TransCan.position);
-        }
-        else if (Input.GetKeyDown(KeyCode.L) && !flag1 && dist3 < 2f)
-        {
-            clone1 = Instantiate(object3, Player.position, Quaternion.identity) as GameObject;
-            flag = true;
-            flag1 = true;
-           // delDis = Vector3.Distance(Player.position, TransCan.position);
-        }
+        
+        //if (Input.GetKeyDown(KeyCode.C) && !flag1 && dist1<2f)
+        //{
+        //   clone1 = Instantiate(object1, Player.position, Quaternion.identity) as GameObject;     
+        //    flagHold = true;
+        //    flag1 = true;
+        //    //delDis= Vector3.Distance(Player.position, TransCan.position);
+
+        //}
+        //else if (Input.GetKeyDown(KeyCode.T) && !flag1 && dist2<2f)
+        //{
+        //    clone1 = Instantiate(object2, Player.position, Quaternion.identity) as GameObject;
+        //    flagHold = true;
+        //    flag1 = true;
+        //    //delDis = Vector3.Distance(Player.position, TransCan.position);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.L) && !flag1 && dist3 < 2f)
+        //{
+        //    clone1 = Instantiate(object3, Player.position, Quaternion.identity) as GameObject;
+        //    flagHold = true;
+        //    flag1 = true;
+        //   // delDis = Vector3.Distance(Player.position, TransCan.position);
+        //}
         delDis = Vector3.Distance(Player.position, TransCan.position);
 
-        if (Input.GetKeyDown(KeyCode.Q) && flag1 && delDis<2f  )
+        if (Input.GetKeyDown(KeyCode.E) && flagHold && delDis<2f  )
         {
             Destroy(clone1);
-            flag1 = false;
-            flag = false;
+            //flag1 = false;
+            flagHold = false;
             delDis=0f;
         }
 
         putDis= Vector3.Distance(Player.position, plate.position);
-        if (Input.GetKeyDown(KeyCode.E) && flag1 && putDis < 2f )
+        if (Input.GetKeyDown(KeyCode.E) && flagHold && putDis < 2f )
         {
             clone1.transform.position = plate.position;
-            flag1 = false;
+           // flag1 = false;
             
             //Destroy(clone1);
-            flag = false;
+            flagHold = false;
         }
-        if (flag)
+
+        if (flagHold)
         {
             
             clone1.transform.position = Vector3.SmoothDamp(clone1.transform.position, Player.position + new Vector3(0, 2, 0), ref AVelocity, smoothTime);
---
+++ Assets/Scripts/CloneObject2.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneObject2 : MonoBehaviour
+{
+    public GameObject object1;
+    public GameObject object2;
+    public GameObject object3;
+    public Transform Player;
+    GameObject clone2;
+    public Transform plate;
+    bool flagHold = false;
+    public float smoothTime = 0.001f;
+    private Vector3 AVelocity = Vector3.zero;
+    //bool flagDestroy = false;
+    public Transform TransCan;
+    float delDis = 3f;
+    float putDis = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist1 = Vector3.Distance(object1.transform.position, Player.position);
+        float dist2 = Vector3.Distance(object2.transform.position, Player.position);
+        float dist3 = Vector3.Distance(object3.transform.position, Player.position);
+
+        //print(dist);
+        if (Input.GetKeyDown(KeyCode.P) && !flagHold)
+        {
+            if (dist1 < 1.5f)
+            {
+                clone2 = Instantiate(object1, Player.position, Quaternion.identity) as GameObject;
+                flagHold = true;
+
+            }
+            else if (dist2 < 1.5f)
+            {
+                clone2 = Instantiate(object2, Player.position, Quaternion.identity) as GameObject;
+                flagHold = true;
+            }
+            else if (dist3 < 1.5f)
+            {
+                clone2 = Instantiate(object3, Player.position, Quaternion.identity) as GameObject;
+                flagHold = true;
+
+            }
+        }
+
+
+        delDis = Vector3.Distance(Player.position, TransCan.position);
+
+        if (Input.GetKeyDown(KeyCode.P) && flagHold && delDis < 2f)
+        {
+            Destroy(clone2);
+            //flag1 = false;
+            flagHold = false;
+            delDis = 0f;
+        }
+
+        putDis = Vector3.Distance(Player.position, plate.position);
+        if (Input.GetKeyDown(KeyCode.P) && flagHold && putDis < 2f)
+        {
+            clone2.transform.position = plate.position;
+
+            flagHold = false;
+        }
+
+        if (flagHold)
+        {
+
+            clone2.transform.position = Vector3.SmoothDamp(clone2.transform.position, Player.position + new Vector3(0, 2, 0), ref AVelocity, smoothTime);
+        }
+
+
+    }
+}
