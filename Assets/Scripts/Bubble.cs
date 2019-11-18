885bc6afc462093aed9063b3ac721904679474c4
MintsF <42982813+MintsF@users.noreply.github.com>
2019-11-18 03:39:11 -0600

    edit menu and player2win position

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -15,15 +15,26 @@ public class Bubble : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        showIngredients();
     }
 
+
+    private string[] final_tag = { "plate_tomato_lettuce_cheese",
+                            "plate_cheeseburger", "plate_steak_salad", "plate_sandwich", "plate_lettuce_burger",
+                            "plate_tomato_sandwich", "plate_CheeseSteak", "plate_tomato_burger", "plate_TomatoSteak"};
     void showIngredients()
     {
         string ingredients = gameObject.tag;
         string[] nameComponent = ingredients.Split('_');
+        bool exist=((IList)final_tag).Contains(ingredients);
+        for (int i =0;i< nameComponent.Length; i++)
+        {
+            
+            Debug.Log("plate content: "+ nameComponent[i]+ "length: "+ nameComponent.Length);
+        }
         
-        
+
+
     }
 
     void checkTag(string str)
