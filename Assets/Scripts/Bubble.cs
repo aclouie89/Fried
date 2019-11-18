277e8e072953140f1e65a515cd420b41a10ede55
MintsF <42982813+MintsF@users.noreply.github.com>
2019-11-18 02:59:45 -0600

    add tied scene

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+
+    public GameObject plate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void showIngredients()
+    {
+        string ingredients = gameObject.tag;
+        string[] nameComponent = ingredients.Split('_');
+        
+        
+    }
+
+    void checkTag(string str)
+    {
+
+    }
+
+}
