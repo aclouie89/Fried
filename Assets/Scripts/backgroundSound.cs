592ab65c4aa6a10ddd48d50e3b3adf50d7a42ac6
MintsF <42982813+MintsF@users.noreply.github.com>
2019-11-23 23:30:56 -0600

    update background sound

---
+++ Assets/Scripts/backgroundSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundSound : MonoBehaviour
+{
+    //public AudioClip bgSound;
+    //private AudioSource source;
+    public SoundVolume sliderValue;
+    public AudioSource source;
+
+    //private void Awake()
+    //{
+    //    source = this.gameObject.AddComponent<AudioSource>();
+    //    source.PlayOneShot(bgSound, 0.5F);
+    //    source.loop = true;
+
+    //}
+
+    //Update is called once per frame
+    void Update()
+    {
+        source.volume = sliderValue.value/2;
+        
+        
+    }
+}
